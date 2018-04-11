52fce351e9dbcb170eb9a4324ada3c09a932a43c
Sophie Obomighie <sophiecodes@gmail.com>
2018-04-11 23:52:48 +0100

    Add files via upload

---
+++ hng.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+  My HNG design
+  <title>
+    Sophie's hng design
+  </title>
+<style type="text/css"
+  </style=""
+</head>
+<body background= "unsplash.jpg">
+  <div class="boxed">
+Welcome to
+Hotels.ng Application page
+
+5:24 pm
+Tuesday, 11th April 2018.
+</div>
+
+</body>
+
+</html>
